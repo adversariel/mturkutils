511bce7465cd6cbbd249e7bfcc074ec13b261cf7
Dan Yamins <dyamins@gmail.com>
2014-07-24 08:17:30 -0400

    compensation stuff

--- experiments/compensation/driver.py
+++ experiments/compensation/driver.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from mturkutils.base import Experiment
 
-userid = 'A1J98D0GPAKZN4'
+userid = 'A1378TVZLWPT2Z'
 class CompensationExperiment(Experiment):
     def createTrials(self):
         self._trials = {'acceptID': [userid]}
@@ -15,24 +15,24 @@ from boto.mturk.connection import MTurkConnection
 conn = MTurkConnection(aws_access_key_id="AKIAI7LNZISMTBL77M3Q", aws_secret_access_key="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")
 
 
-name = "For Sketchloop Special Compensation 0"
+name = "New Sketchloop Special Compensation %s 4" % userid
 description = name
 
-#qual_type = conn.create_qualification_type(name, description, 'Active')
-#qtypeid = qual_type[0].QualificationTypeId
-qtypeid = '3PO9K4KN94Q7ISTQ4FBHJTNC50KY7H'
+
+qual_type = conn.create_qualification_type(name, description, 'Active')
+qtypeid = qual_type[0].QualificationTypeId
 print(qtypeid)
 req = Requirement(qtypeid, 'Exists')
-#conn.assign_qualification(qtypeid, userid, value=1, send_notification=True)
+conn.assign_qualification(qtypeid, userid, value=1, send_notification=True)
 
 exp = CompensationExperiment(htmlsrc = 'compensate.html',
                               htmldst = 'compensate_n%04d.html',
                               sandbox = False,
-                              title = 'Special Compensation',
+                              title = 'Special Compensation for %s, Again 2' % userid,
                               reward = 0,
-                              duration=1500,
-                              description="***Compensation for invited workers only***",
-                              comment="compensation, reimbursement",
+                              duration = 3500,
+                              description = "***Compensation for invited workers only***",
+                              comment = "compensation, reimbursement",
                               collection_name = None,
                               max_assignments=1,
                               bucket_name='sketchloop_special_compensation',
@@ -47,6 +47,3 @@ if __name__ == '__main__':
     exp.testHTMLs()
     exp.uploadHTMLs()
     exp.createHIT()
-
-    #hitids = cPickle.load(open('3ARIN4O78FSZNXPJJAE45TI21DLIF1_2014-06-13_16:25:48.143902.pkl'))
-    #exp.disableHIT(hitids=hitids)
