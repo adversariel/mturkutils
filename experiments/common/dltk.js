b75fdaf263cf7f650fde7ffbaa9d2e5613d9add5
Ha Hong <hahong84@gmail.com>
2014-08-21 22:49:57 -0400

    last commit before restructure of trial loop

--- experiments/common/dltk.js
+++ experiments/common/dltk.js
@@ -6,15 +6,17 @@
     /*************************************************************************
      * Common variables / constants                                          *
      *************************************************************************/
-    dltk.STATLEN = 2;
-    dltk.SLOPPY = 5;
-    dltk.EPS = 2;                     // some slack time in ms for setTimeout2
+    dltk.STATLEN = 2;                 // minimum length to compute statistics
+    dltk.SLOPPY = 5;                  // the amount of shortfall in setTimeout2
+    dltk.EPS = 2;                     // slack time in setTimeout2
     dltk.JS_TRES_TOL = 17;            // ~60Hz frame rate
     dltk.JS_TRES_VAR_TOL = 17 * 17;   // +/- one frame deviation deemed fine
 
-    dltk.js_tres = null;              // setTimeout resolution
-    dltk.js_tres_variance = null;
-    dltk.bogoMIPS = null;
+    dltk.preloaded_rsrcs = {};        // a dictionary of on/off screen contexts + etc. for preloaded imgs
+
+    dltk.js_tres = null;              // setTimeout resolution...
+    dltk.js_tres_variance = null;     // ...and variance
+    dltk.bogoMIPS = null;             // same as the name
 
     var document = window.document;
     var performance = window.performance;
@@ -38,6 +40,18 @@
         else return results[1];
     };
 
+    dltk.wait = function wait(flags, callback) {
+        var wait_inner = function wait_inner() {
+            var finished = true;
+            for (var i = 0; i < flags.length; i++) {
+                if (!flags[i]) finished = false;
+            }
+
+            if (finished) callback();
+            else setTimeout(wait_inner, 0);
+        };
+        setTimeout(wait_inner, 0);
+    };
 
     /*************************************************************************
      * Timing/performance measurement functions                              *
@@ -74,6 +88,8 @@
     dltk.setTimeout2Cnt = 0;  // debugging purposes only. do not rely on this variable.
     dltk.setTimeout2 = function setTimeout2(fn, delay) {
         // A function similar to setTimeout but much more accurate
+        // NOTE: however this does NOT guarantee actual redrawing of the
+        // screen by the browser.
         var t0 = performance.now(), sloppy_delay;
         var setTimeout2_inner = function setTimeout2_inner() {
             while(performance.now() - t0 < delay - dltk.EPS) dltk.setTimeout2Cnt++;
@@ -118,7 +134,7 @@
         // Measures more accurate BogoMIPS by making several measurements
         var meas = [];
         var cnt = 0;
-        var REPS_DEFAULT = 4;
+        var REPS_DEFAULT = 3;
         var REPS = ((typeof(reps) == 'undefined') ? REPS_DEFAULT : reps);
 
         var measureBogoMIPS_inner = function measureBogoMIPS() {
@@ -137,6 +153,7 @@
     };
 
     dltk.runBenchmark = function runBenchmark(callback, tres_dur, mips_reps) {
+        // Runs a suite of benchmarks and calls the "callback"
         dltk.measureTimeResolution(function (js_tres, js_tres_variance) {
             dltk.measureBogoMIPS(function (mips) {
                 if (typeof(callback) == 'function') {
@@ -151,19 +168,25 @@
         }, tres_dur);
     };
 
+
     /*************************************************************************
      * Graphics functions                                                    *
      *************************************************************************/
     dltk.getContextsFromCanvas = function getContextsFromCanvas(onscr_canvas_id) {
-        // double buffering. from: http://blog.bob.sh/2012/12/double-buffering-with-html-5-canvas.html
-        var mainCanvas = document.getElementById(onscr_canvas_id);
-        var ctxMain = mainCanvas.getContext('2d');
-        var ctxOffscreen = dltk.getOffscreenContext(mainCanvas.width, mainCanvas.height);
+        // For double buffering
+        // from: http://blog.bob.sh/2012/12/double-buffering-with-html-5-canvas.html
+        var ctxMain = dltk.getOnScreenContextFromCanvas(onscr_canvas_id);
+        var ctxOffscreen = dltk.getOffScreenContext(ctxMain.canvas.width, ctxMain.canvas.height);
 
         return {onscr: ctxMain, offscr: ctxOffscreen};
     };
 
-    dltk.getOffscreenContext = function getOffscreenContext(w, h) {
+    dltk.getOnScreenContextFromCanvas = function getOnScreenContextFromCanvas(onscr_canvas_id) {
+        var mainCanvas = document.getElementById(onscr_canvas_id);
+        return mainCanvas.getContext('2d');
+    };
+
+    dltk.getOffScreenContext = function getOffScreenContext(w, h) {
         var offscreenCanvas = document.createElement('canvas');
         if (typeof(w) == 'number') offscreenCanvas.width = parseInt(w);
         if (typeof(h) == 'number') offscreenCanvas.height = parseInt(h);
@@ -193,6 +216,60 @@
 
     dltk.copyContexts = function copyContexts(ctxsrc, ctxdst) {
         ctxdst.drawImage(ctxsrc.canvas, 0, 0);
+    };
+
+    dltk.prepareResourcesOnce = function prepareResourcesOnce(url, ctx_onscr, fn) {
+        var w = ctx_onscr.canvas.width, h = ctx_onscr.canvas.height;
+        var ctx_offscr;
+        if (!(url in dltk.preloaded_rsrcs)) dltk.preloaded_rsrcs[url] = {};
+        if (!([w, h] in dltk.preloaded_rsrcs[url])) {
+            ctx_offscr = dltk.getOffScreenContext(w, h);
+            dltk.drawToContext(url, ctx_offscr, fn, false);
+            dltk.preloaded_rsrcs[url][[w, h]] = ctx_offscr;
+        }
+        else setTimeout(fn, 0);    // already processed. skip
+    };
+
+    dltk.prepareResources = function prepareResources(imgFiles, onScrContexts, callback, progressfn) {
+        // Loads resources as specified by "imgFiles" and "responseContexts"
+        // - imgFiles: should be the standard form of ExperimentData.imgFiles for n-AFC tasks
+        //   where an element is a length-2 list of the following form:
+        //      [test img url, [sample img 1 url, sample img 2 url, ..., sample img n url]]
+        // - onScrContexts: a list that contains on-screen contexts where the stimuli will be
+        //   painted onto.  The shape of this must match the that of an "imgFiles" element. 
+
+        var idx = 0;
+        var idx_inner = 0;
+
+        var prepareResources_inner = function prepareResources_inner() {
+            if (idx >= imgFiles.length) {
+                // -- done.
+                if (typeof(callback) == 'function') callback();
+                return;
+            }
+
+            // -- not done
+            // if all inner things for this idx is complete
+            if (idx_inner >= imgFiles[idx][1].length + 1) {
+                idx_inner = 0;
+                idx++;
+                if (typeof(progressfn) == 'function') progressfn(idx, imgFiles.length);
+                setTimeout(prepareResources_inner, 0);
+                return;
+            }
+            // otherwise, run dltk.prepareResourcesOnce().
+            // Next inner iteration will be scheduled inside dltk.prepareResourcesOnce().
+            // process test stimulus:
+            if (idx_inner === 0)
+                dltk.prepareResourcesOnce(imgFiles[idx][0], onScrContexts[0], prepareResources_inner);
+            // process response images:
+            else
+                dltk.prepareResourcesOnce(imgFiles[idx][1][idx_inner - 1], onScrContexts[1][idx_inner - 1],
+                        prepareResources_inner);
+            idx_inner++;
+            // DO NOT SCHEDULE prepareResources_inner() HERE
+        };
+        setTimeout(prepareResources_inner, 0); 
     };
 
 
