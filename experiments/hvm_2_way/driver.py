96514081170b15c14adf4628e272e4627c131843
ardila <ardila@mit.edu>
2014-11-07 15:50:36 -0500

    New task in progress

---
+++ experiments/hvm_2_way/driver.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+import numpy as np
+import cPickle as pk
+import tabular as tb
+import itertools
+import copy
+import sys
+import dldata.stimulus_set.hvm as hvm
+from mturkutils.base import MatchToSampleFromDLDataExperiment
+
+REPEATS_PER_QE_IMG = 4
+ACTUAL_TRIALS_PER_HIT = 150
+
+
+def get_meta():
+    meta_basic = pk.load(open('meta_objt_full_64objs.pkl'))
+    assert len(meta_basic) == 64 * 1000
+
+    cnames = list(meta_basic.dtype.names)
+    cnames.remove('internal_canonical')
+    cnames.remove('texture')        # contains None
+    cnames.remove('texture_mode')   # contains None
+
+    meta = tb.tabarray(columns=[meta_basic[e] for e in cnames],
+                       names=cnames)
+    assert len(meta) == 64 * 1000
+    assert len(np.unique(meta['obj'])) == 64
+    return meta
+
+
+def get_urlbase(obj):
+    return 'https://s3.amazonaws.com/objectome32_final/'
+    # return 'https://s3.amazonaws.com/dicarlocox-rendered-imagesets/' \
+    #        'objectome_cars_subord/'
+
+
+def get_url(obj, idstr, resized=True):
+    if resized:
+        return get_urlbase(obj) + '360x360/' + idstr + '.png'
+    return get_urlbase(obj) + idstr + '.png'
+
+
+def get_url_labeled_resp_img(obj):
+    return get_urlbase(obj) + 'label_imgs/' + obj + '_label.png'
+
+
+def get_subordinate_exp(sandbox=True, debug=False):
+    meta = hvm.meta
+    combs = []
+    for category in np.unique(meta['category']):
+        objs = np.unique(meta[meta['category']==category]['obj'])
+        obj_combs= [e for e in itertools.combinations(objs, 2)]
+        base_url = 'https://s3.amazonaws.com/hvm_timing/'
+        obj_resp_ids = [meta[meta['obj'] == o]['id'][0] for o in objs]
+        response_images = [{
+        'urls': [base_url + obj_id + '.png' for obj_id in obj_resp_ids],
+        'meta': [{'obj': obj, 'category': category} for obj in objs],
+        'labels': objs}]
+        response_images.extend([{
+        'urls': [get_url_labeled_resp_img(o1), get_url_labeled_resp_img(o2)],
+        'meta': [{'obj': o} for o in [o1, o2]],
+        'labels': [o1, o2]
+        } for o1, o2 in obj_combs]
+    urls = [get_url(e['obj'], e['id']) for e in meta]
+
+
+    html_data = {
+        'combs': combs,
+        'response_images': response_images,
+        'num_trials': 125 * 2,
+        'meta_field': 'obj',
+        'meta': meta,
+        'urls': urls,
+        'shuffle_test': True,
+    }
+
+    exp = MatchToSampleFromDLDataExperiment(
+            htmlsrc='web/objectome_64objs_v2.html',
+            htmldst='objectome_64objs_v2_n%05d.html',
+            sandbox=sandbox,
+            title='Object recognition --- report what you see',
+            reward=0.25,
+            duration=1600,
+            keywords=['neuroscience', 'psychology', 'experiment', 'object recognition'],  # noqa
+            description="***You may complete as many HITs in this group as you want*** Complete a visual object recognition task where you report the identity of objects you see. We expect this HIT to take about 10 minutes or less, though you must finish in under 25 minutes.  By completing this HIT, you understand that you are participating in an experiment for the Massachusetts Institute of Technology (MIT) Department of Brain and Cognitive Sciences. You may quit at any time, and you will remain anonymous. Contact the requester with questions or concerns about this experiment.",  # noqa
+            comment='objectome_64objs_v2',
+            collection_name='objectome_64objs_v2',
+            max_assignments=1,
+            bucket_name='objectome_64objs_v2',
+            trials_per_hit=ACTUAL_TRIALS_PER_HIT + 24,  # 150 + 6x4 repeats
+            html_data=html_data,
+            tmpdir='tmp',
+            frame_height_pix=1200,
+            othersrc=['../../lib/dltk.js', '../../lib/dltkexpr.js', '../../lib/dltkrsvp.js'],   # noqa
+            )
+
+    # -- create trials
+    exp.createTrials(sampling='with-replacement', verbose=1)
+    n_total_trials = len(exp._trials['imgFiles'])
+    assert n_total_trials == (64 * 63 / 2) * 250
+    if debug:
+        return exp, html_data
+
+    # -- in each HIT, the followings will be repeated 4 times to
+    # estimate "quality" of data
+    ind_repeats = [0, 4, 47, 9, 17, 18] * REPEATS_PER_QE_IMG
+    rng = np.random.RandomState(0)
+    rng.shuffle(ind_repeats)
+    trials_qe = {e: [copy.deepcopy(exp._trials[e][r]) for r in ind_repeats]
+                 for e in exp._trials}
+
+    # -- flip answer choices of some repeated images
+    n_qe = len(trials_qe['labels'])
+    # if True, flip
+    flips = [True] * (n_qe / 2) + [False] * (n_qe - n_qe / 2)
+    assert len(flips) == n_qe
+    rng.shuffle(flips)
+    assert len(trials_qe.keys()) == 4
+
+    for i, flip in enumerate(flips):
+        if not flip:
+            continue
+        trials_qe['imgFiles'][i][1].reverse()
+        trials_qe['labels'][i].reverse()
+        trials_qe['imgData'][i]['Test'].reverse()
+
+    # -- actual application
+    offsets = np.arange(
+        ACTUAL_TRIALS_PER_HIT - 3, -1,
+        -ACTUAL_TRIALS_PER_HIT / float(len(ind_repeats))
+    ).round().astype('int')
+    assert len(offsets) == len(offsets)
+
+    n_hits_floor = n_total_trials / ACTUAL_TRIALS_PER_HIT
+    n_applied_hits = 0
+    for i_trial_begin in xrange((n_hits_floor - 1) * ACTUAL_TRIALS_PER_HIT,
+                                -1, -ACTUAL_TRIALS_PER_HIT):
+        for k in trials_qe:
+            for i, offset in enumerate(offsets):
+                exp._trials[k].insert(i_trial_begin + offset, trials_qe[k][i])
+                # exp._trials[k].insert(i_trial_begin + offset, 'test')
+        n_applied_hits += 1
+
+    print '** n_applied_hits =', n_applied_hits
+    print '** len for each in _trials =', \
+        {e: len(exp._trials[e]) for e in exp._trials}
+
+    # -- sanity check
+    assert 3360 == n_applied_hits, n_applied_hits
+    assert len(exp._trials['imgFiles']) == 3360 * 174
+    s_ref_labels = [tuple(e) for e in trials_qe['labels']]
+    offsets2 = np.arange(24)[::-1] + offsets
+    ibie = zip(range(0, 3360 * 174, 174), range(174, 3361 * 174, 174))
+    assert all(
+        [[(e1, e2) for e1, e2 in
+         np.array(exp._trials['labels'][ib:ie])[offsets2]] == s_ref_labels
+         for ib, ie in ibie])
+
+    # -- drop unneeded, potentially abusable stuffs
+    del exp._trials['imgData']
+    print '** Finished creating trials.'
+
+    return exp, html_data
+
+
+def main(argv=[], partial=False, debug=False):
+    sandbox = True
+    if len(argv) > 1 and argv[1] == 'production':
+        sandbox = False
+        print '** Creating production HITs'
+    else:
+        print '** Sandbox mode'
+        print '** Enter "driver.py production" to publish production HITs.'
+
+    exp = get_exp(sandbox=sandbox, debug=debug)[0]
+    exp.prepHTMLs()
+    print '** Done prepping htmls.'
+    if partial:
+        return exp
+
+    exp.testHTMLs()
+    print '** Done testing htmls.'
+    exp.uploadHTMLs()
+    exp.createHIT(secure=True)
+    return exp
+
+
+if __name__ == '__main__':
+    main(sys.argv)
